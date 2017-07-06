1a641684abd0890ec9db36b795f991c2e0e42298
emmanbern <emmanbern1@gmail.com>
2017-07-06 06:03:35 -0400

    Structure sans génériques

--- Generics/CircularBuffer.cs
+++ Generics/CircularBuffer.cs
@@ -8,11 +8,26 @@ namespace Generics
 {
     public class CircularBuffer : ICircularBuffer
     {
+
         private double[] _buffer;
         private int _start;
         private int _end;
+        private int _capacity;
 
+        public int Capacity
+        {
+            get { return _buffer.Length; }
+        }
 
+        public bool IsEmpty
+        {
+            get { return _end == _start; }
+        }
+
+        public bool IsFull
+        {
+            get { return (_end + 1) % _buffer.Length == _start; }
+        }
 
         public CircularBuffer() : this(capacity:10)
         {
@@ -23,23 +38,26 @@ namespace Generics
             _buffer = new double[capacity + 1];
             _start = 0;
             _end = 0;
+            _capacity = capacity;
         }
 
-
-        public bool IsFull { get; set; }
-        public bool IsEmpty { get; set; }
-
-        public void Write(double number)
+        public void Write(double value)
         {
-            Console.WriteLine(number);
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+            if(_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+
         }
 
         public double Read()
         {
-            return 1.2;
+            var result = _buffer[_start];
+            _start = (_start + 1) % _buffer.Length;
+            return result;
         }
-
-
 
     }
 }
--- UnitTestProject1/CircularBufferTest.cs
+++ UnitTestProject1/CircularBufferTest.cs
@@ -8,9 +8,41 @@ namespace UnitTestProject1
     public class CircularBufferTest
     {
         [TestMethod]
-        public void Overwrite_When_More_Than_Capacity()
+        public void New_Buffer_Is_Empty()
         {
             var buffer = new CircularBuffer();
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+
+        [TestMethod]
+        public void Three_Element_Buffer_Is_Full_After_Three_Writes()
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+            buffer.Write(1);
+            buffer.Write(1);
+            buffer.Write(1);
+            Assert.IsTrue(buffer.IsFull);
+        }
+
+        [TestMethod]
+        public void First_In_First_Out_When_Not_Full()
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+            var value1 = 1.1;
+            var value2 = 2.3;
+
+            buffer.Write(value1);
+            buffer.Write(value2);
+
+            Assert.AreEqual(value1, buffer.Read());
+            Assert.AreEqual(value2, buffer.Read());
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+
+        [TestMethod]
+        public void Overwrite_When_More_Than_Capacity()
+        {
+            var buffer = new CircularBuffer(capacity : 3);
             var values = new[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
 
             foreach(var value in values)
@@ -20,8 +52,8 @@ namespace UnitTestProject1
 
             Assert.IsTrue(buffer.IsFull);
             Assert.AreEqual(values[2], buffer.Read());
-            Assert.AreEqual(values[2], buffer.Read());
-            Assert.AreEqual(values[2], buffer.Read());
+            Assert.AreEqual(values[3], buffer.Read());
+            Assert.AreEqual(values[4], buffer.Read());
             Assert.IsTrue(buffer.IsEmpty);
         }
     }
