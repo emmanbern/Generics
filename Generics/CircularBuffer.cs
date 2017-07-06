637b41d7fbde5edfe25ea5f3d03bb39bb0fce62a
emmanbern <emmanbern1@gmail.com>
2017-07-05 20:03:01 -0400

    Commit Again

---
+++ Generics/CircularBuffer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class CircularBuffer : ICircularBuffer
+    {
+        private double[] _buffer;
+        private int _start;
+        private int _end;
+
+
+
+        public CircularBuffer() : this(capacity:10)
+        {
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            _buffer = new double[capacity + 1];
+            _start = 0;
+            _end = 0;
+        }
+
+
+        public bool IsFull { get; set; }
+        public bool IsEmpty { get; set; }
+
+        public void Write(double number)
+        {
+            Console.WriteLine(number);
+        }
+
+        public double Read()
+        {
+            return 1.2;
+        }
+
+
+
+    }
+}
---
+++ UnitTestProject1/CircularBufferTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Generics;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CircularBufferTest
+    {
+        [TestMethod]
+        public void Overwrite_When_More_Than_Capacity()
+        {
+            var buffer = new CircularBuffer();
+            var values = new[] { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+            foreach(var value in values)
+            {
+                buffer.Write(value);
+            }
+
+            Assert.IsTrue(buffer.IsFull);
+            Assert.AreEqual(values[2], buffer.Read());
+            Assert.AreEqual(values[2], buffer.Read());
+            Assert.AreEqual(values[2], buffer.Read());
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+    }
+}
