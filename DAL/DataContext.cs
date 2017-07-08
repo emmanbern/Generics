779455085c0559197ab451f0879c624c67ff98f1
emmanbern <emmanbern1@gmail.com>
2017-07-08 00:41:04 -0400

    DataContext instaurer

---
+++ DAL/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
